3670ad2234352f115583ea4eb3e7dda281062a3f
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-10 23:11:11 +0200

    added comments

--- IWA6/challenge2.js
+++ IWA6/challenge2.js
@@ -8,13 +8,27 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
+//declared currency & balance 
+
 let currency = 'R';
+let balance;
+
+// fixed the if statement with bruckects, and removed bugs from the if statement
+
 if (hourOfDay == 00 && minuteOfDay == 00) {
+
+    // since tax was declared as a string because of the %, i had to parse it into an integer and removed 100 from '' since it was a string.
     
-    const taxAsDecimal = parseInt(tax) / 100
-    const startingAfterTax = salary * ( 1 - taxAsDecimal)
-    balance = startingAfterTax - transport - food - rent
+    const taxAsDecimal = parseInt(tax) / 100      
+    const startingAfterTax = salary * ( 1 - taxAsDecimal)  // 1 was spelt as a string.
+
+    // balance and startingAfterTax  was spelt incorrectly
+
+    balance = startingAfterTax - transport - food - rent    
 
 }
 
+// changed the decimals from 3 to 2
 console.log(currency, balance.toFixed(2))
+
+// A quick question, why cant i declare balance inside the if statement?
