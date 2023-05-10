9d5fa901aa85bbaec4737a122a40047bdc0bbb51
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-10 22:47:57 +0200

    added currency to js

---
+++ IWA6/challenge2.js
@@ -0,0 +1,20 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+let currency = 'R';
+if (hourOfDay == 00 && minuteOfDay == 00) {
+    
+    const taxAsDecimal = parseInt(tax) / 100
+    const startingAfterTax = salary * ( 1 - taxAsDecimal)
+    balance = startingAfterTax - transport - food - rent
+
+}
+
+console.log(currency, balance.toFixed(2))
