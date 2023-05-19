e820481dc84877f82d57a1ac143fb7842c778fc2
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-19 13:28:00 +0200

    edited IWA 7-3

--- IWA7/challenge3.js
+++ IWA7/challenge3.js
@@ -13,7 +13,7 @@ const divider = '----------------------------------'
 // multiplied balances by -1 to get a positive value. 
 // Used trim() to remove the leading and trailing white space from LeoSurname and sarahName.
 
-const owed = (( leoBalance * -1) + (sarahBalance* -1)).toFixed(2) 
+const owed = (( leoBalance * -1) + (sarahBalance* -1)).toFixed(2)
 const leo = `${leoName} ${leoSurname.trim()} (Owed : R${(leoBalance * -1).toFixed(2) })` 
 const sarah = `${sarahName.trim()} ${sarahSurname} (Owed : R${(sarahBalance * -1).toFixed(2) })`
 
@@ -24,3 +24,16 @@ const total = "Total amount owed: R"
 const result = leo +'\n' + sarah +'\n' + '\n' + divider +'\n'  + total + owed +'\n' + divider
 
 console.log(result)
+
+
+
+// another way of doing it
+
+// const owed = (parseInt(-1 *  leoBalance) + parseFloat(-1 * sarahBalance)).toFixed(2)
+// const leo = console.log( `${leoName} ${leoSurname} \Owed : R\ ${(- 1* leoBalance).toFixed(2) }`   )
+// const sarah = console.log( `${sarahName} ${sarahSurname} \Owed : R\ ${(-1 * sarahBalance).toFixed(2) }`   )
+
+// const total = "Total amount owed: R"
+// const result =  divider +'\n'  + total + owed +'\n' + divider
+
+// console.log(result)
