5f2753a95f9e9613c944bf7189ea3c4180cf48d3
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-26 12:14:24 +0200

    Completed IWA11

--- IWA11/script.js
+++ IWA11/script.js
@@ -1,32 +1,34 @@
-const 1-root = document(order1)
-const 1-biscuits: document(biscuits)
-const 1-donuts: document(donuts)
-const 1-pancakes: document(pancakes)
-const 1-status: document(status)
+// what need to be done here is replace the (Loading...) with real data from the html files
 
-const 2-root = document(order2)
-const 2-biscuits: document(biscuits)
-const 2-donuts: document(donuts)
-const 2-pancakes: document(pancakes)
-const 2-status: document(status)
+const root1 = document.querySelector('[data-key="order1"]');
+const biscuits1 = root1.querySelector('.biscuits .count');
+const donuts1 = root1.querySelector('.donuts .count');
+const pancakes1 = root1.querySelector('.pancakes .count');
+const status1 = root1.querySelector('.status dd');
 
-const 3-root = document(order3)
-const 3-biscuits: document(biscuits)
-const 3-donuts: document(donuts)
-const 3-pancakes: document(pancakes)
-const 3-status: document(status)
+const root2 = document.querySelector('[data-key="order2"]');
+const biscuits2 = root2.querySelector('.biscuits .count');
+const donuts2 = root2.querySelector('.donuts .count');
+const pancakes2 = root2.querySelector('.pancakes .count');
+const status2 = root2.querySelector('.status dd');
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+const root3 = document.querySelector('[data-key="order3"]');
+const biscuits3 = root3.querySelector('.biscuits .count');
+const donuts3 = root3.querySelector('.donuts .count');
+const pancakes3 = root3.querySelector('.pancakes .count');
+const status3 = root3.querySelector('.status dd');
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+biscuits1.innerHTML= root1.dataset.biscuits;
+donuts1.innerHTML = root1.dataset.donuts;
+pancakes1.innerHTML = root1.dataset.pancakes;
+status1.innerHTML = root1.dataset.delivered ==="true" ? "Delivered" : "Pending";
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+biscuits2.innerHTML= root2.dataset.biscuits;
+donuts2.innerHTML = root2.dataset.donuts;
+pancakes2.innerHTML = root2.dataset.pancakes;
+status2.innerHTML = root2.dataset.delivered ==="true" ? "Delivered" : "Pending";
+
+biscuits3.innerHTML= root3.dataset.biscuits;
+donuts3.innerHTML = root3.dataset.donuts;
+pancakes3.innerHTML = root3.dataset.pancakes;
+status3.innerHTML = root3.dataset.delivered ==="true" ? "Delivered" : "Pending";
