1f9ebb92de9f41ff20f7da8e39913243ec5d47a0
KevinChauke <s.chauke@lab.clicklearning.org>
2023-06-08 10:48:53 +0200

    created challenge 2

--- IWA14/challenge1.js
+++ IWA14/challenge1.js
@@ -2,7 +2,7 @@ firstName = 'John';
 age = 35;
 hobby = 'Coding';
 
-// the problem is that hobby cannot be a function because it's already being declared.
+// the problem is that hobby cannot be a function because it's already being declared as a string.
 // this is an arrow fuction that is called logTwice, it has a parameter called "parameter" which will be logged twice to the console.
 
 const logTwice = (parameter) => {
@@ -10,7 +10,7 @@ const logTwice = (parameter) => {
   console.log(parameter)
 }
 
-// changed "hobby" on the function since it was not a fuction to "parameter"
+// changed "hobby" to "parameter" on the function since it was not a fuction and was declared as a string
 function parameter () {
   logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)       // corrected "name" to "firstName"
 }
