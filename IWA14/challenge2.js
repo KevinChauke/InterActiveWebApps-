ed3fcc42874e0159c194de9e9e1e40201fe44233
KevinChauke <s.chauke@lab.clicklearning.org>
2023-06-09 09:15:35 +0200

    completed challenge 2

--- IWA14/challenge2.js
+++ IWA14/challenge2.js
@@ -1,13 +1,17 @@
 // script.js
 
-function add = a, b => { a + b }
 
-function multiply = a, b => { a - b }
+// this is the declaration so i changed the "function" to "const" and the corrected the arrow function.
+// Removed the curley brackets
+const add =  (a, b) => a + b
+
+const multiply = (a, b) => a * b    		// changed the "-" to "*" since the condition needs to be multiplied.
 
 function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
+	const added = this.add(this.internal.a, this.internal.b)
+	// multiply was declared at the top, so it can't be declared twice, had to change the name to multiplied.
+	const multiplied = this.multiply(added, this.internal.c)   			// changed this to "const" since it's another declaration.  
+	console.log(multiplied)
 }
 
 // Not allowed to change below this
